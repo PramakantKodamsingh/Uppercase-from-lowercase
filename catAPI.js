cf7caad46573fa6aa75af711f4484460e6104974
PramakantKodamsingh <140236716+PramakantKodamsingh@users.noreply.github.com>
2024-02-13 23:49:06 +0530

    Update catAPI.js

--- catAPI.js
+++ catAPI.js
@@ -1,13 +1,13 @@
-
 let btn=document.getElementById("gt")
+let btn1=document.getElementById("scrl")
 let a=document.getElementById("box")
 let b=document.getElementById("ser")
 function get(){
     a.innerHTML=b.value.toUpperCase()
 }
+function refresh(){
+    b.value=''
+
+}
 btn.addEventListener('click',get)
-
-
-
-
-
+btn1.addEventListener('click',refresh)
